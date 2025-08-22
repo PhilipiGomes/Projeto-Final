8476e02bd401b817ee88b951b3322f0ee2c8d302
PhilipiGomes <philipi.rodrigues342@gmail.com>
2025-08-22 08:20:34 -0300

    Terminando Timeline, Profissionalizando o site

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { Timeline } from "@knight-lab/timelinejs";
+
+new Timeline(
+    'timeline-embed', 
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQXcrGD6WJq72ghCr7Cn1kn3fExEF-wln8eEWX-jWFIUFOyEgHufVlvBQelacsufUAUjFe-sTxfy3rp/pubhtml'
+);
